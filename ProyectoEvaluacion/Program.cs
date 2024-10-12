95320a5789306302eee1839a0fcbd98b6b7791bd
francisca <francisca@HABE>
2024-10-11 18:55:18 -0700

    Agregar archivos de proyecto.

---
+++ ProyectoEvaluacion/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ProyectoEvaluacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("introducir calificacion");
+            decimal Calificacion = decimal.Parse(Console.ReadLine());
+
+
+
+            if (Calificacion > 70);
+            {
+
+                Console.WriteLine("califiacion aprovatoria");
+
+            }
+
+             else if (Calificacion > 50 );
+             {
+                    Console.WriteLine("Reprobado con derecho a regularizar");
+                }
+            else (Calificacion < 50 );
+            {
+                Console.WriteLine("Reprobado con deber a recursar");
+            }
+            
+        }
+    }
+}
+
